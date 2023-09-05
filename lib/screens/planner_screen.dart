083dde969481a4906b1fdd1d2b06ecbde3c9f2e6
Amartya Manahto <amartyaipsita135@gmail.com>
2023-09-06 00:12:37 +0530

    new circle

--- lib/screens/planner_screen.dart
+++ lib/screens/planner_screen.dart
@@ -55,10 +55,11 @@ class _PlannerScreenState extends State<PlannerScreen> {
     centerPoint = getCenterScreenPoint(context);
     centerPointAsPoint = Point<num>(centerPoint.dx, centerPoint.dy);
 
-    final ByteData bytes =
+    final ByteData redFlagByte =
         await rootBundle.load('lib/assets/pins/red-flag-hint-pin.png');
-    final Uint8List list = bytes.buffer.asUint8List();
-    await _mapController.addImage("red-flag", list);
+    final Uint8List redFlagList = redFlagByte.buffer.asUint8List();
+    await _mapController.addImage("red-flag", redFlagList);
+
     _mapController.onSymbolTapped.add((argument) {});
   }
 
@@ -78,85 +79,8 @@ class _PlannerScreenState extends State<PlannerScreen> {
             child: Stack(
               children: [
                 map,
-                Visibility(
-                  visible: PTools.pin == tools,
-                  child: Positioned(
-                    left: 0,
-                    bottom: 0,
-                    child: Column(children: [
-                      Padding(
-                          padding:
-                              const EdgeInsets.only(left: 8.0, bottom: 8.0),
-                          child: IconButton.outlined(
-                            isSelected: false,
-                            highlightColor:
-                                const Color.fromARGB(131, 238, 238, 238)
-                                    .withOpacity(0.5),
-                            tooltip: 'red flag',
-                            visualDensity: const VisualDensity(vertical: 3),
-                            onPressed: () {
-                              _placePin(name: 'red-flag');
-                            },
-                            icon: Image.asset(
-                              'lib/assets/pins/red-flag-hint-pin.png',
-                              height: 30,
-                              width: 30,
-                            ),
-                          )),
-                      Padding(
-                          padding:
-                              const EdgeInsets.only(left: 8.0, bottom: 8.0),
-                          child: IconButton.outlined(
-                            isSelected: false,
-                            highlightColor:
-                                const Color.fromARGB(131, 238, 238, 238)
-                                    .withOpacity(0.5),
-                            tooltip: 'yellow flag',
-                            visualDensity: const VisualDensity(vertical: 3),
-                            onPressed: () {},
-                            icon: Image.asset(
-                              'lib/assets/pins/red-flag-hint-pin.png',
-                              height: 30,
-                              width: 30,
-                            ),
-                          )),
-                      Padding(
-                          padding:
-                              const EdgeInsets.only(left: 8.0, bottom: 8.0),
-                          child: IconButton.outlined(
-                            isSelected: false,
-                            highlightColor:
-                                const Color.fromARGB(131, 238, 238, 238)
-                                    .withOpacity(0.5),
-                            tooltip: 'green flag',
-                            visualDensity: const VisualDensity(vertical: 3),
-                            onPressed: () {},
-                            icon: Image.asset(
-                              'lib/assets/pins/red-flag-hint-pin.png',
-                              height: 30,
-                              width: 30,
-                            ),
-                          )),
-                      Padding(
-                          padding:
-                              const EdgeInsets.only(left: 8.0, bottom: 8.0),
-                          child: IconButton.outlined(
-                            isSelected: false,
-                            highlightColor:
-                                const Color.fromARGB(131, 238, 238, 238)
-                                    .withOpacity(0.5),
-                            tooltip: 'blue flag',
-                            visualDensity: const VisualDensity(vertical: 3),
-                            onPressed: () {},
-                            icon: Image.asset(
-                              'lib/assets/pins/red-flag-hint-pin.png',
-                              height: 30,
-                              width: 30,
-                            ),
-                          )),
-                    ]),
-                  ),
-                ),
+                _shapeTools(),
+                _pinTool(),
                 Positioned(
                   right: 0,
                   bottom: 0,
@@ -164,13 +88,18 @@ class _PlannerScreenState extends State<PlannerScreen> {
                     Padding(
                       padding: const EdgeInsets.only(right: 8.0, bottom: 8.0),
                       child: IconButton.filledTonal(
-                          onPressed: () {},
+                          onPressed: () {
+                            _toggleTools(desiredTool: PTools.connector);
+                          },
                           icon: const Icon(Icons.polyline_rounded)),
                     ),
                     Padding(
                       padding: const EdgeInsets.only(right: 8.0, bottom: 8.0),
                       child: IconButton.filledTonal(
-                          onPressed: () {}, icon: const Icon(Icons.shape_line)),
+                          onPressed: () {
+                            _toggleTools(desiredTool: PTools.shape);
+                          },
+                          icon: const Icon(Icons.shape_line)),
                     ),
                     Padding(
                       padding: const EdgeInsets.only(right: 8.0, bottom: 8.0),
@@ -190,6 +119,180 @@ class _PlannerScreenState extends State<PlannerScreen> {
     );
   }
 
+  Visibility _shapeTools() {
+    return Visibility(
+      visible: PTools.shape == tools,
+      child: Positioned(
+        left: 0,
+        bottom: 0,
+        child: Column(children: [
+          Padding(
+              padding: const EdgeInsets.only(left: 8.0, bottom: 8.0),
+              child: IconButton.outlined(
+                isSelected: false,
+                highlightColor:
+                    const Color.fromARGB(131, 238, 238, 238).withOpacity(0.5),
+                tooltip: 'red flag',
+                visualDensity: const VisualDensity(vertical: 3),
+                onPressed: () async {
+
+
+
+
+                  LatLng centerLatlng = await _mapController.toLatLng(centerPointAsPoint);
+                 print('---------------${centerPoint.dx} -------------------${centerPoint.dy}');
+
+                  Point<num> topLeft = Point<num>(
+                      centerPoint.dx - 1, centerPoint.dy + 1);
+                  Point<num> topRight = Point<num>(
+                      centerPoint.dx + 1, centerPoint.dy + 1);
+                  Point<num> bottomLeft = Point<num>(
+                      centerPoint.dx - 1, centerPoint.dy - 1);
+                  Point<num> bottomRight = Point<num>(
+                      centerPoint.dx + 1, centerPoint.dy - 1);
+
+
+
+                  LatLng topLeftLatLng = await _mapController.toLatLng(topLeft);
+                  LatLng topRightLatLng =
+                      await _mapController.toLatLng(topRight);
+                  LatLng bottomLeftLatLng =
+                      await _mapController.toLatLng(bottomLeft);
+                  LatLng bottomRightLatLng =
+                      await _mapController.toLatLng(bottomRight);
+
+                  final ByteData circleByte =
+                      await rootBundle.load('lib/assets/pins/circle.png');
+                  final Uint8List circleList = circleByte.buffer.asUint8List();
+
+
+
+
+                  await _mapController.addImageSource(
+                      "green-circle",
+                      circleList,
+                      LatLngQuad(
+                          topLeft: topLeftLatLng,
+                          topRight: topRightLatLng,
+                          bottomRight: bottomRightLatLng,
+                          bottomLeft: bottomLeftLatLng));
+
+                  await _mapController.addImageLayer('circle-layering', 'green-circle');
+                  setState(() {});
+
+                  // _mapController.addCircle(CircleOptions(
+                  //   circleRadius: 40,
+                  //   // circleBlur: 0.9,
+                  //   circleOpacity: 0.2,
+                  //   circleColor: 'red',
+                  //   circleStrokeWidth: 0.5,
+                  //   geometry: centerLatlng,
+                  //   draggable: true,
+                  // ));
+                },
+                icon: Icon(Icons.crop_square),
+              )),
+          Padding(
+              padding: const EdgeInsets.only(left: 8.0, bottom: 8.0),
+              child: IconButton.outlined(
+                isSelected: false,
+                highlightColor:
+                    const Color.fromARGB(131, 238, 238, 238).withOpacity(0.5),
+                tooltip: 'yellow flag',
+                visualDensity: const VisualDensity(vertical: 3),
+                onPressed: () {},
+                icon: Icon(Icons.circle_outlined),
+              )),
+          Padding(
+              padding: const EdgeInsets.only(left: 8.0, bottom: 8.0),
+              child: IconButton.outlined(
+                isSelected: false,
+                highlightColor:
+                    const Color.fromARGB(131, 238, 238, 238).withOpacity(0.5),
+                tooltip: 'green flag',
+                visualDensity: const VisualDensity(vertical: 3),
+                onPressed: () {},
+                icon: Icon(Icons.polyline_rounded),
+              )),
+        ]),
+      ),
+    );
+  }
+
+  Visibility _pinTool() {
+    return Visibility(
+      visible: PTools.pin == tools,
+      child: Positioned(
+        left: 0,
+        bottom: 0,
+        child: Column(children: [
+          Padding(
+              padding: const EdgeInsets.only(left: 8.0, bottom: 8.0),
+              child: IconButton.outlined(
+                isSelected: false,
+                highlightColor:
+                    const Color.fromARGB(131, 238, 238, 238).withOpacity(0.5),
+                tooltip: 'red flag',
+                visualDensity: const VisualDensity(vertical: 3),
+                onPressed: () {
+                  _putPin(name: 'red-flag');
+                },
+                icon: Image.asset(
+                  'lib/assets/pins/red-flag-hint-pin.png',
+                  height: 30,
+                  width: 30,
+                ),
+              )),
+          Padding(
+              padding: const EdgeInsets.only(left: 8.0, bottom: 8.0),
+              child: IconButton.outlined(
+                isSelected: false,
+                highlightColor:
+                    const Color.fromARGB(131, 238, 238, 238).withOpacity(0.5),
+                tooltip: 'yellow flag',
+                visualDensity: const VisualDensity(vertical: 3),
+                onPressed: () {},
+                icon: Image.asset(
+                  'lib/assets/pins/red-flag-hint-pin.png',
+                  height: 30,
+                  width: 30,
+                ),
+              )),
+          Padding(
+              padding: const EdgeInsets.only(left: 8.0, bottom: 8.0),
+              child: IconButton.outlined(
+                isSelected: false,
+                highlightColor:
+                    const Color.fromARGB(131, 238, 238, 238).withOpacity(0.5),
+                tooltip: 'green flag',
+                visualDensity: const VisualDensity(vertical: 3),
+                onPressed: () {},
+                icon: Image.asset(
+                  'lib/assets/pins/red-flag-hint-pin.png',
+                  height: 30,
+                  width: 30,
+                ),
+              )),
+          Padding(
+              padding: const EdgeInsets.only(left: 8.0, bottom: 8.0),
+              child: IconButton.outlined(
+                isSelected: false,
+                highlightColor:
+                    const Color.fromARGB(131, 238, 238, 238).withOpacity(0.5),
+                tooltip: 'blue flag',
+                visualDensity: const VisualDensity(vertical: 3),
+                onPressed: () {},
+                icon: Image.asset(
+                  'lib/assets/pins/red-flag-hint-pin.png',
+                  height: 30,
+                  width: 30,
+                ),
+              )),
+        ]),
+      ),
+    );
+  }
+
   void _toggleTools({PTools desiredTool = PTools.none}) {
     setState(() {
       if (tools != PTools.none) {
@@ -200,7 +303,7 @@ class _PlannerScreenState extends State<PlannerScreen> {
     });
   }
 
-  void _placePin({required String name}) async {
+  void _putPin({required String name}) async {
     LatLng centerLatlng = await _mapController.toLatLng(centerPointAsPoint);
     pins.add(Symbol(
         '${pins.length}',
